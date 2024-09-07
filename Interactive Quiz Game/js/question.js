050cbc0d80d17d1213475c1714df564e698d76f5
NitishKumar0223 <seth23banarasiya@gmail.com>
2024-09-07 11:58:19 +0530

    Interactive Quiz Game Using HTML CSS JavaScript

---
+++ Interactive Quiz Game/js/question.js
@@ -0,0 +1,63 @@
+//creating an array and passing the number, questions, options and answers.
+
+let questions = [
+    {
+        numb: 1,
+        question:"What does HTML stand for?",
+        answer:"HyperText Markup Language",
+        options: [
+             "HyperText Manipulation Language",
+             "Hyperlink Markup Language",
+             "HyperText Modeling Language",
+             "HyperText Markup Language"
+        ]
+    },
+
+    {
+        numb: 2,
+        question:"Which element to used to give tittle to a table?",
+        answer:"Caption",
+        options: [
+             "Caption",
+             "Headline",
+             "Title",
+             "Heading"
+        ]
+    },
+
+    {
+        numb: 3,
+        question:"What does <td> stand for?",
+        answer:"Table data",
+        options: [
+             "Table database",
+             "Table data",
+             "Table directory",
+             "Table direct row"
+        ]
+    },
+
+    {
+        numb: 4,
+        question:"What does CSS stand for?",
+        answer:"Cascading Style Sheet",
+        options: [
+             "Common Style Sheet",
+             "Colorfull Style Sheet",
+             "Computer Style Sheet",
+             "Cascading Style Sheet"
+        ]
+    },
+
+    {
+        numb: 5,
+        question:"What is the primary use of JavaScript?",
+        answer:"Creating interactive and dynamic web content",
+        options: [
+             "Designing user interface for mobile applications",
+             "Generating server-side code with Node.js",
+             "Creating interactive and dynamic web content",
+             "Analyzing data and generating reports"
+        ]
+    },
+];
